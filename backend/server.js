90effd04f80175e10c5041a09ea7441fed04eb95
Kaua_Matheus <kmsilva3@minha.fag.edu.br>
2025-01-13 23:19:42 -0300

    feat: mudança no banco de dados

--- backend/server.js
+++ backend/server.js
@@ -15,17 +15,24 @@ app.use(express.json())
 app.use(cors())
 
 app.post('/usuarios', async (req, res) => {
-
-    await prisma.user.create({
+    const { name, email, password } = req.body;
+    try {
+      const user = await prisma.user.create({
         data: {
-            email: req.body.email,
-            name: req.body.name,
-            age: req.body.age
-        }
-    })
-    
-    res.status(201).json(req.body)
-})
+          name,
+          email,
+          password,
+        },
+      });
+      res.status(201).json(user);
+    } catch (error) {
+        if (error.code === 'P2002') {
+            return res.status(400).json({ error: "Email já cadastrado" });
+        };
+
+        res.status(400).json({ error: "Erro ao atualizar usuário" });
+    }
+  });
 
 app.get('/usuarios', async (req, res) => {
 
@@ -49,29 +56,41 @@ app.get('/usuarios', async (req, res) => {
 
 app.put('/usuarios/:id', async (req, res) => {
     
+    try {
     await prisma.user.update({
         where: {
-            id: Number(req.params.id)
+            id: req.params.id
         },
         data: {
-            email: req.body.email,
             name: req.body.name,
-            age: req.body.age
+            email: req.body.email,
+            password: req.body.password
         }
     })
     
     res.status(201).json(req.body)
+    } catch (error) {
+        if (error.code === 'P2002') {
+            return res.status(400).json({ error: `O Email ${req.body.email} já está cadastrado!`});
+        }
+
+        return res.status(400).json({ error: "Erro ao atualizar usuário!"});
+    }
 })
 
 app.delete('/usuarios/:id', async (req, res) => {
     
-    await prisma.user.delete({
-        where: {
-            id: Number(req.params.id)
+    try {
+        await prisma.user.delete({
+            where: {
+                id: req.params.id
+            }
+        })
+        
+        res.status(201).json({message: "Usuário excluído com sucesso!"})
+        } catch (error) {
+            return res.status(400).json({ error: "Erro ao excluir usuário!"});
         }
-    })
-    
-    res.status(200).json({message: 'Usuário Deletado com sucesso! '})
 })
 
 app.listen(3000)
--- frontend/src/components/User.tsx
+++ frontend/src/components/User.tsx
@@ -1,4 +1,4 @@
-import React , { useState, useEffect } from "react";
+import React , { useState, useEffect, useRef } from "react";
 import { FaUserCircle } from "react-icons/fa";
 import { IoClose } from "react-icons/io5";
 
@@ -8,6 +8,10 @@ import Button from "./Button";
 const User: React.FC = () => {
     
     const [Opened, setOpened] = useState(false);
+
+    const inputName = useRef<HTMLInputElement>(null);
+    const inputEmail = useRef<HTMLInputElement>(null);
+    const inputPassword = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
         if (Opened) {
@@ -19,9 +23,10 @@ const User: React.FC = () => {
         
     }, [Opened]);
 
-    function Submit() {
-        return 0
+    async function createUsers() {
+        console.log(inputName);
     }
+    
 
     return (
         <div>
@@ -61,35 +66,37 @@ const User: React.FC = () => {
                                 <input
                                     type="text"
                                     id="Nome"
+                                    ref={inputName}
                                     className="border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                                 />
                             </div>
 
-                            <div className="flex flex-col space-y-2">
+                            <div className="flex flex-col space-y-2 mt-1">
                                 <label htmlFor="email" className="text-sm font-medium text-gray-700">
                                     Email
                                 </label>
                                 <input
                                     type="email"
                                     id="email"
+                                    ref={inputEmail}
                                     className="border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                                 />
                             </div>
 
-                            <div className="flex flex-col space-y-2 mt-4">
+                            <div className="flex flex-col space-y-2 mt-1">
                                 <label htmlFor="password" className="text-sm font-medium text-gray-700">
                                     Senha
                                 </label>
                                 <input
                                     type="password"
                                     id="password"
+                                    ref={inputPassword}
                                     className="border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                                 />
                             </div>
 
                             <button
-                                type="submit"
-                                onSubmit={() => Submit()}
+                                onClick={createUsers}
                                 className="w-full mt-6 bg-blue-600 text-white py-2 rounded shadow hover:bg-blue-700"
                             >
                                 Login
@@ -99,7 +106,8 @@ const User: React.FC = () => {
                         {/* Botão para alternar para cadastro */}
                         <p className="text-center text-sm text-gray-600 mt-4">
                             Não tem uma conta?{' '}
-                            <span className="text-blue-600 cursor-pointer hover:underline">
+                            <span 
+                                className="text-blue-600 cursor-pointer hover:underline">
                                 Cadastre-se
                             </span>
                         </p>
